678f61d0557cc89ff0702eb2f5254e383d950dcb
benhuang <ben@dinngo.co>
2021-05-18 14:39:18 +0800

    Fix contract name

--- contracts/handlers/wmatic/HWmatic.sol
+++ contracts/handlers/wmatic/HWmatic.sol
@@ -9,7 +9,7 @@ contract HWmatic is HandlerBase {
     address payable public constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
 
     function getContractName() public pure override returns (string memory) {
-        return "HWMatic";
+        return "HWmatic";
     }
 
     function deposit(uint256 value) external payable {
