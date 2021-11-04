3a17129173b83ad6d12bcbc2a76caaac085bb98c
Mussina Lin <mussina@dinngo.co>
2021-11-04 16:54:55 +0800

    feat: remove staking

--- contracts/staking/IStakingRewardsAdapterRegistry.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-
-interface IStakingRewardsAdapterRegistry {
-    function isValid(address handler) external view returns (bool result);
-    function getInfo(address handler) external view returns (bytes32 info);
-}
