f0fa46029031e3670eac5890382a25255118ffe7
ZD Hu <zd@dinngo.co>
2022-07-21 11:31:54 +0800

    Update curve dao script for beta deployment

--- deploy/09_deploy_hcurvedao.js
+++ deploy/09_deploy_hcurvedao.js
@@ -1,5 +1,9 @@
 const utils = ethers.utils;
 
+// beta parameter
+const registryOwner = '0x64585922a9703d9EdE7d353a6522eb2970f75066';
+const registryAddress = '0x5E56d6c6F763d6B1f21723a11be98533E168C3c9';
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -10,14 +14,52 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log: true,
   });
 
-  const registry = await ethers.getContract('Registry', deployer);
   const hCurveDao = await ethers.getContract('HCurveDao', deployer);
+
+  if (network.name == 'hardhat') {
+    await localDeployment(deployer, hCurveDao);
+  } else {
+    await betaDeployment(hCurveDao);
+  }
+};
+
+async function localDeployment(deployer, hCurveDao) {
+  console.log('local deployment...');
+  const registry = await ethers.getContract('Registry', deployer);
 
   await registry.register(
     hCurveDao.address,
     utils.formatBytes32String('HCurveDao')
   );
-};
+}
+
+async function betaDeployment(hCurveDao) {
+  console.log('beta deployment...');
+
+  const provider = ethers.provider;
+  const [signer] = await ethers.getSigners();
+
+  // register to Registry
+  const iface = new utils.Interface([
+    'function register(address registration,bytes32 info)',
+  ]);
+
+  const registerData = iface.encodeFunctionData('register', [
+    hCurveDao.address,
+    utils.formatBytes32String('HCurveDao'),
+  ]);
+
+  const customData = registerData + 'ff00ff' + registryOwner.replace('0x', '');
+
+  const nonce = await provider.getTransactionCount(registryOwner);
+
+  await signer.sendTransaction({
+    to: registryAddress,
+    nonce: nonce,
+    data: customData,
+    gasLimit: 6000000,
+  });
+}
 
 module.exports.tags = ['HCurveDao'];
 module.exports.dependencies = ['Registry'];
